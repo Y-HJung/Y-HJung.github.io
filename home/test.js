85dd07778fe78a473b3f9421e9a01c689917110d
Y-HJung <79959945+Y-HJung@users.noreply.github.com>
2021-12-02 06:00:06 +0900

    Delete test.js

--- home/test.js
+++
@@ -1,7 +0,0 @@
-let button = document.querySelector('.button');
-let info = document.querySelector('.info');
-
-button.onclick = function() {
-  button.classList.toggle('active');
-  info.classList.toggle('active');
-}
